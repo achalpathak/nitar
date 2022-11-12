6577dacab69024e0d71c9ffb9b24910f19436b29
Akshay Kumar <akshay2796@gmail.com>
2022-11-12 15:05:27 +0530

    Removed App.scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.scss";
+// import "./App.scss";
 import Login from "./pages/login";
 import Home from "./pages/home";
 
