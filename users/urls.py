52c3dcc08ca690b698b9b369146d0a772780ff26
Achal Pathak <pathak.achal5@gmail.com>
2022-12-11 15:03:51 +0530

    fix

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include, re_path
 urlpatterns = [
     re_path(r"get_user_info/$", api.UserInfo.as_view()),
     re_path(r"register/$", api.RegisterUser.as_view()),
-    # re_path(r"update_phone/$", api.UpdatePhoneAPI.as_view()),
+    re_path(r"update_phone/$", api.UpdatePhoneAPI.as_view()),
     re_path(r"send-otp/$", api.SendOTP.as_view()),
     re_path(r"verify-otp/$", api.VerifyOTP.as_view()),
     re_path(r"contact_us/$", api.ContactUsAPI.as_view()),
